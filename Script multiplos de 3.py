a5991c7709ad743796487976cc55940b52c682d9
rosacarla <65525125+rosacarla@users.noreply.github.com>
2021-09-27 05:14:55 -0300

    Incluidos arquivos

---
+++ Script multiplos de 3.py
@@ -0,0 +1,17 @@
+﻿#CURSO: TECNOLOGIA EM ANÁLISE E DESENVOLVIMENTO DE SISTEMAS
+#DISCIPLINA: RACIOCÍNIO COMPUTACIONAL
+#PROFESSOR: LUCAS EMANUEL SILVA E OLIVEIRA
+#ESTUDANTE: CARLA EDILA S R SILVEIRA
+#AVALIAÇÃO REGULAR PRESENCIAL (ARP)
+#DATA DE ENTREGA: 07.05.2020
+
+#Algoritmo com laços de repetição
+#lista dos múltiplos de 3
+
+for multiplo in range(3,1000,3): #Estebelece lista a percorrer pelo for.
+    if multiplo % 3 == 0: #Condição para selecionar nº divisível por 3 com resto 0.
+        print(multiplo) #Impressão da lista na tela.
+
+#Final do código
+
+
